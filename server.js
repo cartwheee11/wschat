14d8d3dee67457dd28fb4129c288a02b4b2995d9
Cartwheee11 <beardedstump@yandex.ru>
2020-12-08 05:56:10 +0300

    5051

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ updateHistory();
 
 let clients = new Set();
 
-let server = new ws.Server({port : process.env.PORT || 50501});
+let server = new ws.Server({port : 5051});
 
 function handleMessage(message, ws) {
 	message = JSON.parse(message);
